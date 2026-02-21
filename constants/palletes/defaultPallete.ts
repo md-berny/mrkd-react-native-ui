8f9b6e5fc977c07933e9629ad95f3fa502e7ed0a
Michael Dave Sumang <michael@berny.com.au>
2026-02-21 22:13:39 +0800

    alias loopym pallete as default pallete

---
+++ constants/palletes/defaultPallete.ts
@@ -0,0 +1,1 @@
+export { loopymPallete as defaultPallete } from "./loopym/loopymPallete";
--- constants/styles/borderRadius/borderRadius.ts
+++ constants/styles/borderRadius/borderRadius.ts
@@ -1,11 +1,12 @@
 export const borderRadius = {
   none: 0,
   sm: 2,
-  DEFAULT: 4,
   md: 6,
   lg: 8,
   xl: 12,
   "2xl": 16,
   "3xl": 24,
+  "4xl": 32,
   full: 9999,
+  DEFAULT: 8,
 } as const;
